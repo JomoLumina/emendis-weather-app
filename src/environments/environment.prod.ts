58d4eb56caf573a3735221bc0ccd6c8c3459825c
Jomo Lumina <luminajj@yahoo.com>
2022-09-20 07:12:58 +0200

    added prod variables

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
-  production: true
+  production: true,
+  weatherApiBaseUrl: 'https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline',
+  apiKey: "EUXGC7PLND5TWN45BG9SKFK2Q",
+  cacheExpiresIn: 300000 //expires in 5 minutes
 };
